1d92c834b194fe029cb776b22ea50e6ccdbd7604
Heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-06-10 14:19:51 +0800

    Add more syntax kinds

--- src/Mamba/Code Analysis/Syntax/SyntaxKind.h
+++ src/Mamba/Code Analysis/Syntax/SyntaxKind.h
@@ -4,15 +4,62 @@ MAMBA_NAMESPACE_BEGIN
 
 enum class SyntaxKind
 {
-    // Tokens
     BadToken,
+
+    // Tokens
     EndOfFileToken,
     NumberToken,
     StringToken,
     PlusToken,
+    PlusEqualsToken,
     MinusToken,
+    MinusEqualsToken,
     StarToken,
-    SlashToken
+    StarEqualsToken,
+    SlashToken,
+    SlashEqualsToken,
+    BangToken,
+    EqualsToken,
+    TildeToken,
+    HatToken,
+    HatEqualsToken,
+    AmpersandToken,
+    AmpersandAmpersandToken,
+    AmpersandEqualsToken,
+    PipeToken,
+    PipeEqualsToken,
+    PipePipeToken,
+    EqualsEqualsToken,
+    BangEqualsToken,
+    LessToken,
+    LessOrEqualsToken,
+    GreaterToken,
+    GreaterOrEqualsToken,
+    OpenParenthesisToken,
+    CloseParenthesisToken,
+    OpenBraceToken,
+    CloseBraceToken,
+    ColonToken,
+    CommaToken,
+    IdentifierToken,
+
+    // Keywords
+    BreakKeyword,
+    ContinueKeyword,
+    ElseKeyword,
+    FalseKeyword,
+    ForKeyword,
+    FunctionKeyword,
+    IfKeyword,
+    LetKeyword,
+    VarKeyword,
+    ReturnKeyword,
+    ToKeyword,
+    TrueKeyword,
+    WhileKeyword,
+    DoKeyword,
+
+    CompilationUnit
 };
 
 MAMBA_NAMESPACE_END
