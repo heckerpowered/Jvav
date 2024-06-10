896796c1d75ccd3caf634e715443226024d5f5c7
Heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-06-10 17:19:56 +0800

    Add syntax node

---
+++ src/Mamba/Code Analysis/Syntax/SyntaxNode.cpp
@@ -0,0 +1,64 @@
+#include "SyntaxNode.h"
+#include "MambaCore.h"
+
+#include <memory>
+#include <ranges>
+
+MAMBA_NAMESPACE_BEGIN
+
+SyntaxNode::SyntaxNode(const std::shared_ptr<const class SyntaxTree>& SyntaxTree) noexcept : SyntaxTree(SyntaxTree) {}
+
+const std::shared_ptr<const class SyntaxNode> SyntaxNode::Parent() const noexcept
+{
+    return SyntaxTree->GetParent(*this);
+}
+
+TextSpan SyntaxNode::Span() const noexcept
+{
+    const auto First = Children().front()->Span();
+    const auto Last = Children().back()->Span();
+    return TextSpan::FromBounds(First.Start, Last.End());
+}
+
+TextSpan SyntaxNode::FullSpan() const noexcept
+{
+    const auto First = Children().front()->FullSpan();
+    const auto Last = Children().back()->FullSpan();
+    return TextSpan::FromBounds(First.Start, Last.End());
+}
+
+TextLocation SyntaxNode::Location() const noexcept
+{
+    return TextLocation(SyntaxTree->Text, Span());
+}
+
+std::vector<std::shared_ptr<const SyntaxNode>> SyntaxNode::AncestorsAndSelf() const noexcept
+{
+    auto Ancestors = std::vector<std::shared_ptr<const SyntaxNode>>();
+    auto Node = shared_from_this();
+    while (Node)
+    {
+        Ancestors.emplace_back(Node);
+        Node = Node->Parent();
+    }
+
+    return Ancestors;
+}
+
+std::vector<std::shared_ptr<const SyntaxNode>> SyntaxNode::Ancestors() const noexcept
+{
+    return AncestorsAndSelf() | std::views::drop(1) | std::ranges::to<std::vector>();
+}
+
+std::shared_ptr<const class SyntaxToken> SyntaxNode::LastToken() const noexcept
+{
+    if (auto Token = std::dynamic_pointer_cast<class SyntaxToken>(shared_from_this()))
+    {
+        return Token;
+    }
+
+    // A syntax node should always contain at least 1 token.
+    return Children().back()->LastToken();
+}
+
+MAMBA_NAMESPACE_END
---
+++ src/Mamba/Code Analysis/Syntax/SyntaxNode.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "MambaCore.h"
+#include "SyntaxKind.h"
+#include "TextLocation.h"
+#include "TextSpan.h"
+
+#include <memory>
+#include <vector>
+
+MAMBA_NAMESPACE_BEGIN
+
+class SyntaxNode : std::enable_shared_from_this<SyntaxNode>
+{
+protected:
+    [[nodiscard]] SyntaxNode(const std::shared_ptr<const class SyntaxTree>& SyntaxTree) noexcept;
+
+public:
+    const std::shared_ptr<const class SyntaxTree> SyntaxTree;
+
+    [[nodiscard]] const NullableSharedPtr<const class SyntaxNode> Parent() const noexcept;
+
+    [[nodiscard]] virtual SyntaxKind Kind() const noexcept = 0;
+    [[nodiscard]] virtual TextSpan Span() const noexcept;
+    [[nodiscard]] virtual TextSpan FullSpan() const noexcept;
+    [[nodiscard]] TextLocation Location() const noexcept;
+
+    [[nodiscard]] std::vector<std::shared_ptr<const SyntaxNode>> AncestorsAndSelf() const noexcept;
+    [[nodiscard]] std::vector<std::shared_ptr<const SyntaxNode>> Ancestors() const noexcept;
+    [[nodiscard]] virtual std::vector<std::shared_ptr<const SyntaxNode>> Children() const noexcept = 0;
+
+    [[nodiscard]] std::shared_ptr<const class SyntaxToken> LastToken() const noexcept;
+};
+
+MAMBA_NAMESPACE_END
