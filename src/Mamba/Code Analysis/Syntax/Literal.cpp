cd8b6de577fe778321b2e62a2d1bfce2dd474eb5
Heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-06-15 15:04:30 +0800

    Make LiteralValue trivally destructive & Add default constructor to literal & add StringValue to Literal

---
+++ src/Mamba/Code Analysis/Syntax/Literal.cpp
@@ -0,0 +1,10 @@
+#include "Literal.h"
+#include "MambaCore.h"
+
+MAMBA_NAMESPACE_BEGIN
+
+LiteralValue::LiteralValue() noexcept {}
+
+Literal::Literal() noexcept {}
+
+MAMBA_NAMESPACE_END
--- src/Mamba/Code Analysis/Syntax/Literal.h
+++ src/Mamba/Code Analysis/Syntax/Literal.h
@@ -2,12 +2,13 @@
 
 #include "MambaCore.h"
 #include <cstdint>
+#include <memory>
 
 MAMBA_NAMESPACE_BEGIN
 
 union LiteralValue
 {
-    String StringValue;
+    StringView StringValue;
     Char CharacterValue;
 
     std::uint8_t UnsignedByteValue;
@@ -24,6 +25,20 @@ union LiteralValue
     float FloatValue;
 
     bool BooleanValue;
+
+    LiteralValue() noexcept;
+
+private:
+    // In general, for some types of literal such as strings, this type only stores a view of the value, and the value
+    // is generally stored in the objects that holds this object. To ensure the validity of the view, this object can
+    // only be copied when the object that holds this object is copied.
+    //
+    // For example, Literal has a StringValue member variable, then the StringValue of this type is its view. This
+    // object can be only copied if the Literal object is copied.
+    LiteralValue(const LiteralValue&) = default;
+    LiteralValue(LiteralValue&&) = default;
+
+    friend class Literal;
 };
 
 enum class LiteralType
@@ -53,6 +68,10 @@ struct Literal
 {
     LiteralValue Value;
     LiteralType Type;
+
+    std::optional<std::shared_ptr<const String>> StringValue;
+
+    [[nodiscard]] Literal() noexcept;
 };
 
 MAMBA_NAMESPACE_END
