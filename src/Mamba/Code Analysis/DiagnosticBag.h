37e2680fd457c7a6d3033a1c4a60880d964d3333
Heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-06-17 21:54:04 +0800

    Add 'noexcept' qualifier and add more report functions

--- src/Mamba/Code Analysis/DiagnosticBag.h
+++ src/Mamba/Code Analysis/DiagnosticBag.h
@@ -16,13 +16,18 @@ public:
     void AddRange(const std::vector<std::shared_ptr<const class Diagnostic>>& Diagnostics) noexcept;
 
 private:
-    void ReportError(const TextLocation Location, const std::shared_ptr<const String> Message);
-    void ReportWarning(const TextLocation Location, const std::shared_ptr<const String> Message);
-    void ReportInformation(const TextLocation Location, const std::shared_ptr<const String> Message);
+    void ReportError(const TextLocation Location, const std::shared_ptr<const String> Message) noexcept;
+    void ReportWarning(const TextLocation Location, const std::shared_ptr<const String> Message) noexcept;
+    void ReportInformation(const TextLocation Location, const std::shared_ptr<const String> Message) noexcept;
 
 public:
-    void ReportInvalidCharacter(const TextLocation Location, const Char Character);
-    void ReportUnterminatedString(const TextLocation Location);
+    void ReportInvalidCharacter(const TextLocation Location, const Char Character) noexcept;
+    void ReportUnterminatedString(const TextLocation Location) noexcept;
+
+    void ReportInvalidDecimal(const TextLocation Location, const StringView Literal) noexcept;
+    void ReportInvalidHexadecimal(const TextLocation Location, const StringView Literal) noexcept;
+    void ReportInvalidBinary(const TextLocation Location, const StringView Literal) noexcept;
+    void ReportInvalidOctal(const TextLocation Location, const StringView Literal) noexcept;
 };
 
 MAMBA_NAMESPACE_END
