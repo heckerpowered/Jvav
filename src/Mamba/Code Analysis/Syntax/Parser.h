79033da2287b23724e1b1c6f3455de316693441f
Heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-06-18 17:37:30 +0800

    Implement basic parser

--- src/Mamba/Code Analysis/Syntax/Parser.h
+++ src/Mamba/Code Analysis/Syntax/Parser.h
@@ -3,17 +3,31 @@
 #include "CompilationUnitSyntax.h"
 #include "DiagnosticBag.h"
 #include "MambaCore.h"
+#include "SyntaxKind.h"
+#include "SyntaxToken.h"
 #include <memory>
 
 MAMBA_NAMESPACE_BEGIN
 
 class Parser
 {
+    const std::shared_ptr<const class SyntaxTree> SyntaxTree;
+    const std::shared_ptr<const class SourceText> Text;
+    const std::vector<std::shared_ptr<class SyntaxToken>> Tokens;
+    std::size_t Position;
+
 public:
     DiagnosticBag Diagnostics;
 
-    [[nodiscard]] Parser(const std::shared_ptr<const class SyntaxTree> SyntaxTree);
+    [[nodiscard]] Parser(const std::shared_ptr<const class SyntaxTree> SyntaxTree) noexcept;
 
+private:
+    [[nodiscard]] std::shared_ptr<class SyntaxToken> Peek(const std::size_t Offset) noexcept;
+    [[nodiscard]] std::shared_ptr<class SyntaxToken> Current() noexcept;
+    [[nodiscard]] std::shared_ptr<class SyntaxToken> NextToken() noexcept;
+    [[nodiscard]] std::shared_ptr<class SyntaxToken> MatchToken(const SyntaxKind Kind) noexcept;
+
+public:
     [[nodiscard]] std::shared_ptr<const class CompilationUnitSyntax> ParseCompilationUnit() noexcept;
 };
 
