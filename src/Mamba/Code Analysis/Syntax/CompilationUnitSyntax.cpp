f1ec984213ebb75c738b0ab0af5cd68e978fd30e
Heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-06-20 17:39:27 +0800

    Remove unnecessary cast

--- src/Mamba/Code Analysis/Syntax/CompilationUnitSyntax.cpp
+++ src/Mamba/Code Analysis/Syntax/CompilationUnitSyntax.cpp
@@ -19,7 +19,7 @@ namespace Mamba
 
     std::vector<std::shared_ptr<const SyntaxNode>> CompilationUnitSyntax::Children() const noexcept
     {
-        return { std::dynamic_pointer_cast<const SyntaxNode>(EndOfFileToken) };
+        return { EndOfFileToken };
     }
 
 } // namespace Mamba
