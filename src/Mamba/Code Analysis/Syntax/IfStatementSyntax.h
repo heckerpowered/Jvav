af23fcfa94e4548c852526e78e414f3272fded31
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-07-01 21:13:17 +0800

    Add 'IfStatementSyntax'

---
+++ src/Mamba/Code Analysis/Syntax/IfStatementSyntax.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "StatementSyntax.h"
+
+#include <memory>
+
+namespace Mamba
+{
+    class IfStatementSyntax : public StatementSyntax
+    {
+    public:
+        using Super = StatementSyntax;
+
+        [[nodiscard]] IfStatementSyntax(const std::shared_ptr<const class SyntaxTree> SyntaxTree,
+                                        const std::shared_ptr<const class SyntaxToken> IfKeyword,
+                                        const NullableSharedPtr<const class SyntaxToken> OpenParenthesisToken,
+                                        const std::shared_ptr<const class ExpressionSyntax> Condition,
+                                        const NullableSharedPtr<const class SyntaxToken> CloseParenthesisToken,
+                                        const std::shared_ptr<const class StatementSyntax> ThenStatement,
+                                        const NullableSharedPtr<const class ElseClauseSyntax> ElseClause) noexcept;
+
+        virtual SyntaxKind Kind() const noexcept override;
+        virtual std::vector<std::shared_ptr<const class SyntaxNode>> Children() const noexcept override;
+
+        const std::shared_ptr<const class SyntaxToken> IfKeyword;
+        const NullableSharedPtr<const class SyntaxToken> OpenParenthesisToken;
+        const std::shared_ptr<const class ExpressionSyntax> Condition;
+        const NullableSharedPtr<const class SyntaxToken> CloseParenthesisToken;
+        const std::shared_ptr<const class StatementSyntax> ThenStatement;
+        const NullableSharedPtr<const class ElseClauseSyntax> ElseClause;
+    };
+} // namespace Mamba
