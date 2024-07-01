c2ba7d9f6957b55a8530ecc9be6352819a884e6e
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-07-01 14:06:27 +0800

    Add member syntax

---
+++ src/Mamba/Code Analysis/Syntax/MemberSyntax.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "SyntaxNode.h"
+#include <memory>
+
+namespace Mamba
+{
+    class MemberSyntax : public SyntaxNode
+    {
+    public:
+        using Super = SyntaxNode;
+
+    protected:
+        MemberSyntax(const std::shared_ptr<const class SyntaxTree> SyntaxTree) noexcept;
+    };
+} // namespace Mamba
