f4703d60f46930ef54272280232dd07e930ca996
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-07-01 21:13:01 +0800

    Add 'FunctionDeclarationSyntax'

---
+++ src/Mamba/Code Analysis/Syntax/FunctionDeclarationSyntax.cpp
@@ -0,0 +1,50 @@
+#include "FunctionDeclarationSyntax.h"
+
+#include "BlockStatementSyntax.h"
+#include "ParameterSyntax.h"
+#include "SyntaxToken.h"
+#include "TypeClauseSyntax.h"
+
+namespace Mamba
+{
+    FunctionDeclarationSyntax::FunctionDeclarationSyntax(
+        const std::shared_ptr<const class SyntaxTree> SyntaxTree,
+        const std::shared_ptr<const class SyntaxToken> FunctionKeyword,
+        const std::shared_ptr<const class SyntaxToken> Identifier,
+        const std::shared_ptr<const class SyntaxToken> OpenParenthesisToken,
+        const std::shared_ptr<const SeperatedSyntaxList<std::shared_ptr<const class ParameterSyntax>>> Parameters,
+        const std::shared_ptr<const class SyntaxToken> CloseParenthesisToken,
+        const NullableSharedPtr<const class TypeClauseSyntax> Type,
+        const std::shared_ptr<const class BlockStatementSyntax> Body) noexcept :
+        Super(SyntaxTree),
+        FunctionKeyword(FunctionKeyword),
+        Identifier(Identifier),
+        OpenParenthesisToken(OpenParenthesisToken),
+        Parameters(Parameters),
+        CloseParenthesisToken(CloseParenthesisToken),
+        Type(Type),
+        Body(Body)
+    {
+    }
+
+    SyntaxKind FunctionDeclarationSyntax::Kind() const noexcept
+    {
+        return SyntaxKind::FunctionDeclaration;
+    }
+
+    std::vector<std::shared_ptr<const class SyntaxNode>> FunctionDeclarationSyntax::Children() const noexcept
+    {
+        auto Children = std::vector<std::shared_ptr<const class SyntaxNode>>();
+        Children.reserve(6 + Parameters->Count() * 2);
+
+        Children.emplace_back(FunctionKeyword);
+        Children.emplace_back(Identifier);
+        Children.emplace_back(OpenParenthesisToken);
+        Children.append_range(Parameters->WithSeperators());
+        Children.emplace_back(CloseParenthesisToken);
+        Children.emplace_back(Type);
+        Children.emplace_back(Body);
+
+        return Children;
+    }
+} // namespace Mamba
---
+++ src/Mamba/Code Analysis/Syntax/FunctionDeclarationSyntax.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "MemberSyntax.h"
+#include "SeperatedSyntaxList.h"
+#include "SyntaxKind.h"
+
+#include <memory>
+
+namespace Mamba
+{
+    class FunctionDeclarationSyntax : public MemberSyntax
+    {
+    public:
+        using Super = MemberSyntax;
+
+        [[nodiscard]] FunctionDeclarationSyntax(
+            const std::shared_ptr<const class SyntaxTree> SyntaxTree,
+            const std::shared_ptr<const class SyntaxToken> FunctionKeyword,
+            const std::shared_ptr<const class SyntaxToken> Identifier,
+            const std::shared_ptr<const class SyntaxToken> OpenParenthesisToken,
+            const std::shared_ptr<const SeperatedSyntaxList<std::shared_ptr<const class ParameterSyntax>>> Parameters,
+            const std::shared_ptr<const class SyntaxToken> CloseParenthesisToken,
+            const NullableSharedPtr<const class TypeClauseSyntax> Type,
+            const std::shared_ptr<const class BlockStatementSyntax> Body) noexcept;
+
+        virtual SyntaxKind Kind() const noexcept override;
+        virtual std::vector<std::shared_ptr<const class SyntaxNode>> Children() const noexcept override;
+
+        const std::shared_ptr<const class SyntaxToken> FunctionKeyword;
+        const std::shared_ptr<const class SyntaxToken> Identifier;
+        const std::shared_ptr<const class SyntaxToken> OpenParenthesisToken;
+        const std::shared_ptr<const SeperatedSyntaxList<std::shared_ptr<const class ParameterSyntax>>> Parameters;
+        const std::shared_ptr<const class SyntaxToken> CloseParenthesisToken;
+        const NullableSharedPtr<const class TypeClauseSyntax> Type;
+        const std::shared_ptr<const class BlockStatementSyntax> Body;
+    };
+} // namespace Mamba
