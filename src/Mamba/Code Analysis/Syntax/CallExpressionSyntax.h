fd5585cdb5d05fef21c84128b13fb3510554de58
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-07-01 12:30:40 +0800

    Add CallExpressionSyntax

---
+++ src/Mamba/Code Analysis/Syntax/CallExpressionSyntax.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "ExpressionSyntax.h"
+#include "SeperatedSyntaxList.h"
+#include "SyntaxNode.h"
+#include "SyntaxToken.h"
+#include <memory>
+
+namespace Mamba
+{
+    class CallExpressionSyntax : public ExpressionSyntax
+    {
+    public:
+        using Super = ExpressionSyntax;
+
+        [[nodiscard]] CallExpressionSyntax(
+            const std::shared_ptr<const class SyntaxTree> SyntaxTree,
+            const std::shared_ptr<const class SyntaxToken> Identifier,
+            const std::shared_ptr<const class SyntaxToken> OpenParenthesisToken,
+            const std::shared_ptr<const SeperatedSyntaxList<std::shared_ptr<const class ExpressionSyntax>>> Arguments,
+            const std::shared_ptr<const class SyntaxToken> CloseParenthesisToken) noexcept;
+
+        virtual SyntaxKind Kind() const noexcept override;
+        virtual std::vector<std::shared_ptr<const class SyntaxNode>> Children() const noexcept override;
+
+        const std::shared_ptr<const class SyntaxToken> Identifier;
+        const std::shared_ptr<const class SyntaxToken> OpenParenthesisToken;
+        const std::shared_ptr<const SeperatedSyntaxList<std::shared_ptr<const class ExpressionSyntax>>> Arguments;
+        const std::shared_ptr<const class SyntaxToken> CloseParenthesisToken;
+    };
+} // namespace Mamba
