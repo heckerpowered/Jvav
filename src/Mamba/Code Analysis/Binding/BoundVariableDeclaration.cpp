caf27dd8aef2dc77d0884480ecef8c8a09404771
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-07-28 00:02:16 +0800

    implement kind function

--- src/Mamba/Code Analysis/Binding/BoundVariableDeclaration.cpp
+++ src/Mamba/Code Analysis/Binding/BoundVariableDeclaration.cpp
@@ -3,10 +3,15 @@
 using namespace Mamba;
 
 BoundVariableDeclaration::BoundVariableDeclaration(
-    const std::shared_ptr<const class SyntaxNode> Syntax,
-    const std::shared_ptr<const class VariableSymbol> Variable,
-    const std::shared_ptr<const class BoundExpression> Initializer
+    const std::shared_ptr<const SyntaxNode> Syntax,
+    const std::shared_ptr<const VariableSymbol> Variable,
+    const std::shared_ptr<const BoundExpression> Initializer
 ) noexcept :
     Super(Syntax), Variable(Variable), Initializer(Initializer)
 {
+}
+
+BoundNodeKind BoundVariableDeclaration::Kind() const noexcept
+{
+    return BoundNodeKind::VariableDeclaration;
 }
