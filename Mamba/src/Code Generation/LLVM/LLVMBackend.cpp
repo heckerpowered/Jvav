9025137e70cb02c646dbc46503054f0212b63ed3
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-10-11 23:16:03 +0800

    添加EmitLLVM选系那个

--- Mamba/src/Code Generation/LLVM/LLVMBackend.cpp
+++ Mamba/src/Code Generation/LLVM/LLVMBackend.cpp
@@ -1,3 +1,4 @@
+#include "MambaOptions.h"
 #include <ranges>
 #include <source_location>
 #include <string>
@@ -387,15 +388,22 @@ void LLVMBackend::GenerateCode(std::span<BoundCompilationUnit*> CompilationUnits
     LLVMModule.setDataLayout(TargetMachine->createDataLayout());
     LLVMModule.setTargetTriple(TargetTriple);
 
-    LLVMModule.print(errs(), nullptr);
-
-    auto FileName = fast_io::concat(ModuleName, ".o");
+    using namespace std::string_view_literals;
+    auto FileName = fast_io::concat(ModuleName, Options::EmitLLVM ? ".ll"sv : ".o"sv);
 
     auto error_code = std::error_code();
     raw_fd_ostream Out(FileName, error_code, sys::fs::OF_None);
     if (error_code)
     {
         Error("无法打开输出文件: ", error_code.message());
+        return;
+    }
+
+    if (Options::EmitLLVM)
+    {
+        LLVMModule.print(Out, nullptr);
+        Out.flush();
+        fast_io::io::println("编译成功: ", FileName);
         return;
     }
 
