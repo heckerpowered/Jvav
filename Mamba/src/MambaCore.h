48c0678c7768317f862c2f85bd435e4ee03b5618
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-10-13 17:50:34 +0800

    修复Error不应该是panic

--- Mamba/src/MambaCore.h
+++ Mamba/src/MambaCore.h
@@ -147,12 +147,9 @@ namespace Mamba
     }
 
     template<typename... T>
-    [[noreturn]] void Error(T&&... Args) noexcept
+    void Error(T&&... Args) noexcept
     {
-        fast_io::io::perrln(
-            Color("错误: ", Colors::BrightForegroundRed),
-            std::forward<T>(Args)...
-        );
+        fast_io::io::perrln(Color("错误: ", Colors::BrightForegroundRed), std::forward<T>(Args)...);
     }
 
     // Hatcher stores a callable object, so that the result of the call can be emplace constructed into a container by
