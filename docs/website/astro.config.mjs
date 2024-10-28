3385387e8e5debf198dee01fa748079df8470cab
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-10-28 20:23:06 +0800

    Update astro.config.mjs
    Signed-off-by: heckerpowered <72550834+heckerpowered@users.noreply.github.com>

--- docs/website/astro.config.mjs
+++ docs/website/astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://heckerpowered.github.io',
-    base: 'jvav/docs/website',
+    base: '/docs/website',
 })
