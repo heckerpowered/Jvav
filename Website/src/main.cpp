28328f94fa6a5c3a06cbedf722323efdbddf1343
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-10-28 19:50:29 +0800

    Jvav网站测试

--- Website/src/main.cpp
+++ Website/src/main.cpp
@@ -1,6 +1,31 @@
-#include <boost/asio.hpp>
 #include <fast_io.h>
+#include <fast_io_device.h>
+#include <vector>
 
 int main()
+try
 {
+    fast_io::net_service service;
+    std::vector<fast_io::native_socket_file> sockets;
+
+    for (auto i : std::views::iota(0, 65535))
+    {
+        try
+        {
+            fast_io::native_socket_file socket(fast_io::tcp_listen(i));
+        }
+        catch (fast_io::error error)
+        {
+            fast_io::io::perrln("Failed to listen on port ", i, ", ", error);
+        }
+    }
+
+    while (true)
+    {
+        fast_io::io::scan("");
+    }
+}
+catch (fast_io::error error)
+{
+    fast_io::io::perrln(error);
 }
---
+++ docs/website/astro.config.mjs
@@ -0,0 +1,9 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://heckerpowered.github.io',
+    base: 'jvav',
+})
