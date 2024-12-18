bc9677a8b26a32788331cd6c9411905111fbab0e
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-12-18 19:49:45 +0800

    Add comments to the preprocessor

--- Mamba/src/main.cpp
+++ Mamba/src/main.cpp
@@ -2,6 +2,8 @@
 
 using namespace Mamba;
 
+// The unit test needs Mamba, the entry point of Mamba causes
+// the multiple definition of main function.
 #if !defined(MAMBA_TEST)
 
 int main(int argc, char* argv[])
