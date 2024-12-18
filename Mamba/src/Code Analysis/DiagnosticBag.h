c77d0fd87bcde42d9d9d760680cb51f75b697253
heckerpowered <72550834+heckerpowered@users.noreply.github.com>
2024-12-18 21:53:07 +0800

    添加注释 (#49)
    * Update LLVM
    
    * Update concat information
    
    * Remove website
    
    * Remove website
    
    * Remove website
    
    * Move all parameters to a line
    
    * Update dependencies
    
    * Add '-j$nproc' to build command
    
    * Add comments to the preprocessor
    
    * Add comments
    
    * Add comment

--- Mamba/src/Code Analysis/DiagnosticBag.h
+++ Mamba/src/Code Analysis/DiagnosticBag.h
@@ -36,31 +36,114 @@ namespace Mamba
         }
 
     public:
+        /**
+         * Report a invalid character.
+         * e.g. "Invalid character 'x'."
+         */
         void ReportInvalidCharacter(TextLocation Location, Char Character) noexcept;
+
+        /**
+         * Report a string literal is unterminated.
+         * e.g. "Unterminated string literal."
+         */
         void ReportUnterminatedString(TextLocation Location) noexcept;
 
+        /**
+         * Report a invalid decimal literal.
+         * e.g. "Invalid decimal literal 'x'."
+         */
         void ReportInvalidDecimal(TextLocation Location, StringView Literal) noexcept;
+
+        /**
+         * Report a invalid hexadecimal literal.
+         * e.g. "Invalid hexadecimal literal 'x'."
+         */
         void ReportInvalidHexadecimal(TextLocation Location, StringView Literal) noexcept;
+
+        /**
+         * Report a invalid binary literal.
+         * e.g. "Invalid binary literal 'x'."
+         */
         void ReportInvalidBinary(TextLocation Location, StringView Literal) noexcept;
+
+        /**
+         * Report a invalid octal literal.
+         * e.g. "Invalid octal literal 'x'."
+         */
         void ReportInvalidOctal(TextLocation Location, StringView Literal) noexcept;
 
+        /**
+         * Report a unexpected token, specialize information based on expected type.
+         * e.g. "Unexpected Token 'x', Expected: 'y'"
+         * e.g. "There should be an identifier." (When expected identifier token)
+         */
         void ReportUnexpectedToken(TextLocation Location, SyntaxKind Kind, SyntaxKind ExpectedKind) noexcept;
 
+        /**
+         * Report that the value of the expression is discarded, usually found in expression statements.
+         * e.g. "the expression value is discarded"
+         */
         void ReportDiscardExpressionValue(TextLocation Location) noexcept;
+
+        /**
+         * Report error that a variable is already declared when declare a variable.
+         * e.g. "Variable 'x' is already declared at here: <line number>:<start character>"
+         */
         void ReportVariableAlreadyDeclared(TextLocation Location, StringView Name) noexcept;
+
+        /**
+         * Report the code in the specified location is unreachable.
+         * e.g. "Unreachable code."
+         */
         void ReportUnreachableCode(TextLocation Location) noexcept;
+
+        /**
+         * Report the code is unreachable, the location to be reported is depends on the node.
+         * When the node is a block statement, report the first statement, if there's no statements
+         * in the block statement, this function do nothing. For all other situations, report the whole
+         * expression or statement.
+         */
         void ReportUnreachableCode(const SyntaxNode* Node) noexcept;
 
+        /**
+         * Report error for undeclared identifier is used.
+         * e.g. "Undeclared identifier 'x'"
+         */
         void ReportUndeclaredIdentifier(TextLocation Location, StringView Name) noexcept;
+
+        /**
+         * Report error for ambiguous identifier, cannot select the best candidate in the overload set.
+         */
         void ReportAmbiguousIdentifier(TextLocation Location, StringView Name) noexcept;
 
+        /**
+         * Report error for unexpected type, expected type and actual type.
+         * e.g. "Expected type 'int', but got type 'float'."
+         */
         void ReportTypeMismatch(TextLocation Location, const TypeSymbol& ExpectedType, const TypeSymbol& ActualType) noexcept;
 
+        /**
+         * Report error unary expression, which the operator is not defined for the given type.
+         * e.g. "Undefined unary operator '-' for type 'string'."
+         */
         void ReportUndefinedUnaryOperator(TextLocation Location, const SyntaxToken* OperatorToken, const TypeSymbol& OperandType) noexcept;
+
+        /**
+         * Report error binary expression, which the operator is not defined for the given types.
+         * e.g. "Undefined binary operator '+' for types 'int' and 'float'."
+         */
         void ReportUndefinedBinaryOperator(TextLocation Location, const TypeSymbol& LeftType, const SyntaxToken* OperatorToken, const TypeSymbol& RightType) noexcept;
 
+        /**
+         * Report error for trying to assign to an immutable variable.
+         * e.g. "Cannot assign to variable 'x' because it is immutable."
+         */
         void ReportVariableImmutable(TextLocation Location, StringView Name) noexcept;
 
+        /**
+         * Report error for function call with wrong argument count, expected count and actual count,
+         * e.g. "Expected 2 arguments, but got 3."
+         */
         void ReportArgumentCountMismatch(TextLocation Location, std::size_t ExpectedCount, std::size_t ActualCount) noexcept;
     };
 
